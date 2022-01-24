dbee148e330550005fd181b14aa5f4792663e29a
ohaeori <0typhoon416@gmail.com>
2022-01-24 21:49:21 +0900

    create_notice

--- notice_demo/notice/urls.py
+++ notice_demo/notice/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('notice_list/', views.notice_list, name='notice_list'),
+    path('notice_list/create_notice/', views.create_notice),
 ]
--- notice_demo/notice/views.py
+++ notice_demo/notice/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Notice
 
 # Create your views here.
+#처음화면
 def index(request):
     return render(request,'notice/index.html')
 
+#게시글 목록 표시
 def notice_list(request): 
     notice_list=Notice.objects.all()
     return render(request, 'notice/notice_list.html',{'notice_list':notice_list})
+
+#게시글 작성
+def create_notice(request):
+    if request.method == 'POST':
+        new_article=Notice.objects.create(
+                postname=request.POST['title'],
+                contents=request.POST['contents'],
+            )
+        return redirect('/notice_list/')
+    return render(request, 'notice/create_notice.html')
