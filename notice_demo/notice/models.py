d0cfedca05402b9d0fd5fadf482f013a933a7eb8
ohaeori <0typhoon416@gmail.com>
2022-01-24 21:29:00 +0900

    noticelist

--- notice_demo/notice/models.py
+++ notice_demo/notice/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from sympy import content
 
 # Create your models here.
+
+class Notice(models.Model):
+    title=models.CharField(max_length=30)
+    contents = models.TextField()
+
+    def __str__(self):return self.title
--- notice_demo/notice/views.py
+++ notice_demo/notice/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Notice
 
 # Create your views here.
 def index(request):
     return render(request,'notice/index.html')
 
-def notice_list(request): return render(request, 'notice/notice_list.html')
+def notice_list(request): 
+    notice_list=Notice.objects.all()
+    return render(request, 'notice/notice_list.html',{'notice_list':notice_list})
