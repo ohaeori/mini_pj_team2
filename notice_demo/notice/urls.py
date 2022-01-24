69f4a7ea326889983f58a411a98a440e92303251
ohaeori <0typhoon416@gmail.com>
2022-01-24 21:18:27 +0900

    notice_list

--- notice_demo/notice/urls.py
+++ notice_demo/notice/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('notice_list/', views.notice_list, name='notice_list'),
 ]
--- notice_demo/notice/views.py
+++ notice_demo/notice/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'notice/index.html')
+
+def notice_list(request): return render(request, 'notice/notice_list.html')
