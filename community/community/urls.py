cf4e941c4d886e739914ea86bf4899cc8c89c9ce
ohaeori <0typhoon416@gmail.com>
2022-01-25 21:23:08 +0900

    notice

---
+++ community/community/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('notice_list/', views.notice_list, name='notice_list'),
+    path('notice_list/create_notice/', views.create_notice),
+    path('notice_list/<int:pk>/', views.posting,name='posting'),
+    path('notice_list/<int:pk>/delete_notice/', views.delete_notice),
+]
