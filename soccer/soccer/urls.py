c09995e1adbc0562bb4812d89ae4b9519399c5a8
ohaeori <0typhoon416@gmail.com>
2022-01-26 02:25:23 +0900

    signin/out

--- soccer/soccer/urls.py
+++ soccer/soccer/urls.py
@@ -2,12 +2,16 @@ from django.urls import path,include
 
 from . import views
 
-
+appname='soccer'
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.login_custom, name='login_custom'),
+    path('logout/custom/', views.logout_custom, name='logout_custom'),
+    path('signup/custom/', views.signup_custom, name='signup_custom'),
     path('community/', views.community, name='index'),
     path('community/notice_list/', views.notice_list, name='notice_list'),
     path('community/notice_list/create_notice/', views.create_notice),
     path('community/notice_list/<int:pk>/', views.posting,name='posting'),
     path('community/notice_list/<int:pk>/delete_notice/', views.delete_notice),
+    
 ]
--- soccer/soccer/views.py
+++ soccer/soccer/views.py
@@ -118,3 +118,59 @@ def delete_notice(request, pk):
 
 
 
+# user 출력
+def user(request):
+   user_list = USER.objects.all()
+   return render(
+        request,
+        'soccer/line.html',
+        {'user_list': user_list }
+   )
+
+
+#회원가입
+def signup_custom(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('u_id')
+        user_pw = request.POST.get('pw')
+        user_name = request.POST.get('u_name')
+        birth_date = request.POST.get('birth_date')
+        nickname = request.POST.get('nickname')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+
+        u = USER(
+            u_id=user_id, pw=user_pw, u_name=user_name, 
+            birth_date=birth_date, nickname=nickname, phone_num=phone_num, email=email)
+        u.date_joined = timezone.now()
+        u.save()
+        return redirect('../../')
+    else:
+        return render(request, 'soccer/signup_custom.html')
+
+#로그인
+def login_custom(request):
+    user_list = USER.objects.all()
+    if request.method == 'POST':
+        user_id = request.POST.get('u_id')
+        user_pw = request.POST.get('pw')
+        try:
+            u = USER.objects.get(u_id=user_id, pw=user_pw)
+            # 회원정보 조회 실패 시 예외 발생
+        except:
+            return HttpResponse('로그인 실패')
+        else: #로그인 판단 요소 ==> 나중에 이걸로 다양한 정보 확인(동현에이블러님 활용)
+            request.session['u_id'] = u.u_id
+            request.session['u_name'] = u.u_name
+        return render(request,'soccer/login_custom.html',{'user_list': user_list }
+    )   
+      
+    else:
+        return render(request, 'soccer/login_custom.html')
+
+#로그아웃
+def logout_custom(request):
+    del request.session['u_id'] # 개별 삭제
+    del request.session['u_name'] # 개별 삭제
+    request.session.flush() # 전체 삭제
+    return redirect('../../')
