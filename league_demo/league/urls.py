c9e4cc1d353f02aba70d4aef211911eca57f5aeb
ohaeori <0typhoon416@gmail.com>
2022-01-26 21:43:10 +0900

    cc

--- league_demo/league/urls.py
+++ league_demo/league/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
    
-    path('player/', views.player_search),  # 추천순 정렬 url
+    path('player/', views.player_search), 
     path('club/', views.select_club),
     path('club/<str:c_name>/', views.club_info),
     path('main/<str:date>/', views.main),
