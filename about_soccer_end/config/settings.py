ec45068aa0b761d663bc9c9c7ac9604452f455e6
juna101010 <mafal201121@gmail.com>
2022-01-28 14:16:57 +0900

    Update settings.py

--- about_soccer_end/config/settings.py
+++ about_soccer_end/config/settings.py
@@ -113,13 +113,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+# TIME_ZONE = 'UTC'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
+
+
+# USE_TZ = True
+
+USE_TZ = False
 
 USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
